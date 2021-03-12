dd11a7a8da44cf72bce1657604da41e34617703b
Francis Le Roy <francis.leroy@protonmail.ch>
2021-03-12 15:14:41 +0100

    Better init_db for tests, copying database rather than applying the migrations each times

--- src/tests/run_migrations.rs
+++ src/tests/run_migrations.rs
@@ -1,6 +1,7 @@
 use super::*;
 use lazy_static::lazy_static;
 use std::str::FromStr;
+use std::sync::Mutex;
 use uuid::Uuid;
 
 mod embedded_migrations {
@@ -19,6 +20,8 @@ lazy_static! {
             .connect_lazy(&BASILIQ_DATABASE_URL)
             .expect("to initialize the management Postgres connection pool")
     };
+    static ref BASILIQ_DEFAULT_DATABASE: String = { format!("basiliq_test_{}", Uuid::new_v4()) };
+    static ref BASILIQ_DEFAULT_DATABASE_INIT: Mutex<bool> = { Mutex::new(false) };
 }
 
 pub async fn run_migrations(db_name: &str) {
@@ -32,26 +35,40 @@ pub async fn run_migrations(db_name: &str) {
 }
 
 pub async fn init_db() -> (String, sqlx::PgPool) {
+    {
+        let mut init_bool = BASILIQ_DEFAULT_DATABASE_INIT
+            .lock()
+            .expect("the database management mutex is poisoned");
+        {
+            sqlx::query(
+                format!("CREATE DATABASE \"{}\";", BASILIQ_DEFAULT_DATABASE.as_str()).as_str(),
+            )
+            .execute(&*BASILIQ_MANAGEMENT_POOL)
+            .await
+            .expect("to create a new database");
+            run_migrations(BASILIQ_DEFAULT_DATABASE.as_str()).await;
+            *init_bool = true;
+        }
+    }
     let db_name = format!("basiliq_test_{}", Uuid::new_v4());
     println!("Gen name {}", db_name);
-    sqlx::query(format!("CREATE DATABASE \"{}\";", db_name.as_str()).as_str())
-        .execute(&*BASILIQ_MANAGEMENT_POOL)
-        .await
-        .expect("to create a new database");
-    println!("Created database");
-
-    run_migrations(db_name.as_str()).await;
-    println!("Running migrations");
-
+    sqlx::query(
+        format!(
+            "CREATE DATABASE \"{}\" WITH TEMPLATE \"{}\";",
+            db_name.as_str(),
+            BASILIQ_DEFAULT_DATABASE.as_str()
+        )
+        .as_str(),
+    )
+    .execute(&*BASILIQ_MANAGEMENT_POOL)
+    .await
+    .expect("to create a new database");
     let conn_opt = sqlx::postgres::PgConnectOptions::from_str(&BASILIQ_DATABASE_URL)
         .expect("to parse the basiliq database url")
         .database(db_name.as_str());
-    println!("Connecting");
     let pool = sqlx::pool::PoolOptions::new()
         .min_connections(1)
         .max_connections(3)
-        .connect_with(conn_opt)
-        .await
-        .expect("to initialize the management Postgres connection pool");
+        .connect_lazy_with(conn_opt);
     (db_name, pool)
 }
