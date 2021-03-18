7d64bb1589e161afff69663c191bf691f8b600eb
Francis Le Roy <francis.leroy@protonmail.ch>
2021-03-18 18:44:47 +0100

    Added query params test for delete

--- src/tests/delete/mod.rs
+++ src/tests/delete/mod.rs
@@ -1,3 +1,39 @@
 use super::*;
 
 mod delete_success;
+mod query_params;
+
+async fn test_delete<'a>(
+    transaction: &mut sqlx::Transaction<'_, sqlx::Postgres>,
+    query_end: &str,
+    _test_name: &str,
+    _data: &BTreeMap<String, Vec<Uuid>>,
+) -> Vec<sqlx::postgres::PgRow> {
+    let ciboulette_store = gen_bag();
+    let table_store = gen_table_store(&ciboulette_store);
+    let parsed_url = Url::parse(format!("http://localhost{}", query_end).as_str()).unwrap();
+    const INTENTION: CibouletteIntention = CibouletteIntention::Delete;
+
+    let req_builder = CibouletteRequestBuilder::new(INTENTION, &parsed_url, &None);
+    let request = req_builder.build(&ciboulette_store).unwrap();
+    let ciboulette_request = CibouletteDeleteRequest::try_from(request).unwrap();
+    let builder = Ciboulette2PostgresBuilder::gen_delete(
+        &ciboulette_store,
+        &table_store,
+        &ciboulette_request,
+    )
+    .unwrap();
+    let (query, args) = builder.build().unwrap();
+
+    let raw_rows: Vec<sqlx::postgres::PgRow> = sqlx::query_with(&query, args)
+        .fetch_all(&mut *transaction)
+        .await
+        .unwrap();
+    snapshot_table(
+        &mut *transaction,
+        "db_snapshot_delete_while_testing_query_params",
+        &["peoples", "people-article", "favorite_color"],
+    )
+    .await;
+    raw_rows
+}
---
+++ src/tests/delete/query_params.rs
@@ -0,0 +1,3 @@
+use super::*;
+
+ciboulette_query_test_single!(test_delete);
